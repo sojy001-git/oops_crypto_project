56142cc89c4ab3985580c9c1da7779aa66efd148
sojy001-git <sojy001@naver.com>
2025-03-24 18:20:57 +0900

    Update entrypoint-scheduler.sh

--- entrypoint-scheduler.sh
+++ entrypoint-scheduler.sh
@@ -4,5 +4,14 @@
 export PATH="/opt/airflow/venv/bin:$PATH"
 export AIRFLOW_HOME=/opt/airflow
 
+# ✅ DB가 초기화 안 됐으면 init 실행
+if [ ! -f /opt/airflow/initialized ]; then
+    echo "Initializing Airflow DB..."
+    airflow db init
+    touch /opt/airflow/initialized
+else
+    echo "Airflow DB already initialized."
+fi
+
 # ✅ Airflow 스케줄러 실행
 exec airflow scheduler
